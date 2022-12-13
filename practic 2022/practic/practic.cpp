31f8b210efa0c5104cc3ad3070ef022717310399
fantazer23 <takvando2014@gmail.com>
2022-12-13 20:04:23 +0300

    ff

---
+++ practic 2022/practic/practic.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+   setlocale(LC_ALL, "");
+    int n;
+    cout << "Введіть число n" << endl;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0) 
+        {
+            cout << "Дільник: " << i << endl;
+        }
+    }
+    return 0;
+}
